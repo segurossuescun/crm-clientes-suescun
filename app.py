6675bb5fc0f4ea258094859b2866178bffa5fc7e
Adriana Suescun <segurossuescun@gmail.com>
2025-06-18 20:39:52 -0400

    🚀 Corrijo ruta principal para que Render abra el index.

--- app.py
+++ app.py
@@ -31,9 +31,10 @@ def buscar_clientes():
 # ✅ Ruta principal para mostrar index.html desde /static
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('static', 'index.html')
+
+import os
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
